7ff7e001bd8324f6734e6d23149657c0ed12e53d
ali alghubari <alghubari.772017@gmail.com>
2021-09-21 10:31:17 +0300

    add money

---
+++ Money/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Money.Data;
+using Money.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Money.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationTypeController (ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            IEnumerable<ApplicationType> objlist = _db.applicationTypes;
+            return View(objlist);
+        }
+
+        public IActionResult Create()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+      
+
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            var obj = _db.applicationTypes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            var obj = _db.applicationTypes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(ApplicationType obj)
+
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _db.applicationTypes.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(ApplicationType obj)
+
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _db.applicationTypes.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create( ApplicationType obj)
+        {
+            _db.applicationTypes.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Money/Controllers/CategoryController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Money.Data;
+using Money.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Money.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            IEnumerable<Category> objlist = _db.Category;
+            return View(objlist);
+        }
+
+        public IActionResult Create()
+        {
+
+           
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create( Category obj)
+
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+           
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0){
+                return NotFound();
+
+            }
+            var obj = _db.Category.Find(id);
+            if(obj==null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(Category obj)
+
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Category.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+
+        }
+
+
+    }
+
+}
